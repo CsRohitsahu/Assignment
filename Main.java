23f2b2b612c2b0c3dea10738a64034cf2d5eed71
CsRohitsahu <121692543+CsRohitsahu@users.noreply.github.com>
2022-12-31 12:46:31 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,38 +1,32 @@
 import java.util.Scanner;
-
-import java.util.Scanner;
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        // taking 2 integer valur in two variables x and y and print their product
-        System.out.println("Enter value of x");
-        int x=sc.nextInt();
-        System.out.println("Enter value of y");
-        int y =sc.nextInt();
-        int p=x*y;
-        System.out.println("product of "+x+" and "+ y +" is "+p);
-        //Q2 print the ASCII value of charater 'u'
-        int a='U';
-        System.out.println("Ascii value of U is "+a);
-        //Q3 printing area of rectangle 
-        System.out.println("Enter length and bridth of rectangle");
-        int ln=sc.nextInt();
-        int br=sc.nextInt();
-        System.out.println("area of rectangle is "+ln*br);
-        //q3 calculating cube of a number
-        System.out.println("enter no:");
-        int n=sc.nextInt();
-        System.out.println("cube of "+n+" is "+n*n*n);
-        //Q5 swaping number using third variable
-        int n1,n2;
-        n1=3;
-        n2=8;
-        int temp;
-        temp =n1;
-        n1=n2;
-        n2=temp;
-        System.out.println("n1= "+n1+" n2=" +n2);
+      /*  System.out.println("enter name ");
+        String nm=sc.next();
+        System.out.println("enter roll");
+        int roll=sc.nextInt();
+        System.out.println("enter field of interest");
+        String interest=sc.next();
+        System.out.println("Name: "+nm+"\nRoll number: "+roll+"\nField of interest: "+interest);*/
 
+       /* System.out.println("Enter ist stirng ");
+        String s1=sc.nextLine();
+        System.out.println("Enter 2nd string");
+        String s2=sc.nextLine();
+        System.out.println(s1+s2);*/
 
+        /*System.out.println("enter mark in first subject:");
+        float m1=sc.nextFloat();
+        System.out.println("enter mark in seond subject");
+        float m2=sc.nextFloat();
+        System.out.println("Enter mark in third subject");
+        float m3=sc.nextFloat();
+        float total=m1+m2+m3;
+        float percent =total/3;
+        System.out.println("total mark: "+total+"\npercentage: "+percent+"%");*/
+
+        
     }
+    
 }
